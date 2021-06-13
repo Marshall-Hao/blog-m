ff437e38a2c3861247726500bcf13ee0cec86889
marshall chen <marshallchan666@outlook.com>
2021-06-14 05:44:42 +0800

    ref needs value

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,26 @@
 <template>
-
+  <div class="home">
+    <h1>Home</h1>
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name">
+  </div>
 </template>
 
 <script>
-
+import { ref } from 'vue'
 export default {
   name: 'Home',
-  components: {
-  }
+  setup() {
+    const name = ref('mario')
+    const age = ref(30)
+    // const p = ref(null)
+    const handleClick = (e) => {
+      name.value = 'luigi'
+      age.value = 35
+    }
+    return { name, age, handleClick }
+  },
 }
 </script>
